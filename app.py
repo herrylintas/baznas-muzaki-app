970c73bd110d366272221a572e6728738968d422
herrylintas <herrylintas@gmail.com>
2026-07-23 22:45:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+Python
+import streamlit as st
 
+st.markdown(
+    """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
